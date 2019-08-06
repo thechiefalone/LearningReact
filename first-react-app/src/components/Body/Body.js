2417e2f9d4e02c5a9f761287016bb98e3751a92a
thechiefalone <alperenozkan@gmail.com>
2019-08-06 10:51:45 +0200

    update

--- first-react-app/src/components/Body/Body.js
+++ first-react-app/src/components/Body/Body.js
@@ -2,6 +2,17 @@ import React from "react";
 import "./Body.css";
 
 class Body extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      InputValue: ""
+    };
+  }
+  ChangeTitle(event) {
+    this.setState({
+      InputValue: event.target.value
+    });
+  }
   render() {
     return (
       <div className="Body">
@@ -10,6 +21,17 @@ class Body extends React.Component {
         <p id="body-paragraph">
           This is the course for learning Reactjs Library
         </p>
+        <form className="form">
+          <input
+            onChange={e => {
+              this.ChangeTitle(e);
+            }}
+            type="text"
+            name="input"
+            placeholder="Enter A Title"
+          />
+        </form>
+        <p className="inputParagraph" />
       </div>
     );
   }
