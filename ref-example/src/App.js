35200032ca823f77fe4f76b143f561261e15cd63
thechiefalone <alperenozkan@gmail.com>
2019-08-22 11:20:08 +0200

    exercise changed to firstName and lastName

--- ref-example/src/App.js
+++ ref-example/src/App.js
@@ -4,24 +4,29 @@ import "./App.css";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.textInput = React.createRef();
+    this.firstName = React.createRef();
+    this.lastName = React.createRef();
     this.state = {
-      value: ""
+      firstName: "",
+      lastName: ""
     };
   }
   handleSubmit = e => {
     e.preventDefault();
     this.setState({
-      value: this.textInput.current.value
+      firstName: this.firstName.current.value,
+      lastName: this.lastName.current.value
     });
   };
   render() {
     return (
       <div>
         <h1>React Ref - createRef</h1>
-        <h3>Value: {this.state.value}</h3>
+        <h3>First Name: {this.state.firstName}</h3>
+        <h3>Last Name: {this.state.lastName}</h3>
         <form onSubmit={this.handleSubmit}>
-          <input type="text" ref={this.textInput} />
+          <input type="text" ref={this.firstName} />
+          <input type="text" ref={this.lastName} />
           <button>Submit</button>
         </form>
       </div>
